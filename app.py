b1eceb802e9ee7a2c21a82aa8d8f7e5777eed632
Naira06 <naira.abdelrasoul@eng-st.cu.edu.eg>
2022-11-02 22:58:40 +0200

    prime

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import pandas as pd
 import  streamlit_vertical_slider  as svs
 
 st.set_page_config(page_title="Equalizer", page_icon=":headphones:",layout="wide")
-
 
 hide_st_style = """
             <style>
@@ -12,6 +12,46 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+button_style = """
+        <style>
+        .stButton > button {
+            width: 90px;
+            height: 35px;
+        }
+        </style>
+        """
+st.markdown(button_style, unsafe_allow_html=True)
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        upload_file_plceholder=st.empty()
+        upload_file=upload_file_plceholder.file_uploader("Browse", type=["csv"], key="uploader")    
+    
+  #declare then in function function
+def open_csv():
+    signal_upload=pd.read_csv(upload_file)
+    return signal_upload
+def open_mp3():
+    if upload_file.type!="mp3":
+        st.error(" wrong Extension, Requierd *.mp3* ")
+    else:
+      Audio=st.audio(upload_file, format='audio/mp3')
+    return Audio
+with  col2:
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
+    st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
+    choose=st.radio("",("Sine wave","Music","Vowels","Biomedical Signal"))
+    if choose =="Sine wave" or choose =="Biomedical Signal":
+        upload_file_plceholder.file_uploader("Browse", type=["csv"])    
+        if upload_file:
+          open_csv()
+    elif choose =="Music" or choose =="Vowels":
+        upload_file_plceholder.file_uploader("Browse", type=["mp3"])    
+        if upload_file:
+          open_mp3()
+
+
 
 groups = [('slider1',50),
             ('slider2',150),
@@ -24,7 +64,6 @@ groups = [('slider1',50),
             ('slider9',850),
             ('slider10',950),
 ]
-
 boundary = int(50)
 adjusted_data = []
 sliders = {}
@@ -40,3 +79,11 @@ for idx, i in enumerate(groups):
         if sliders[f'slider_group_{key}'] == None:
             sliders[f'slider_group_{key}']  = i[1]
         adjusted_data.append((i[0],sliders[f'slider_group_{key}'] ))
+#with st.container():
+play,pause= st.columns([0.5,5])
+with play:
+    play_btn=st.button("Play")
+with pause:
+    pause_btn=st.button("pause")
+
+    
